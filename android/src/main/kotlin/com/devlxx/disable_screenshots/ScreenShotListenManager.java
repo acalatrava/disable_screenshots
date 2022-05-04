cd689235b01f5d67fadd8428d1355d6ac17d2aff
Antonio Calatrava <antonio@antoniocalatrava.com>
2022-05-04 14:44:57 +0200

    import

--- android/src/main/kotlin/com/devlxx/disable_screenshots/ScreenShotListenManager.java
+++ android/src/main/kotlin/com/devlxx/disable_screenshots/ScreenShotListenManager.java
@@ -7,6 +7,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
